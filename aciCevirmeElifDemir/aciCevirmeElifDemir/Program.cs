641fb3208508c4a1ac5abc78018e5c8697aac4ba
ellifdemir23 <95495081+ellifdemir23@users.noreply.github.com>
2022-12-03 20:08:42 +0300

    Add files via upload

---
+++ aciCevirmeElifDemir/aciCevirmeElifDemir/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aciCevirmeElifDemir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d, r, g;
+            double pi = 3.14;
+
+            Console.WriteLine("Açıyı giriniz = ");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            r = Convert.ToInt32(d * pi / 180);
+            g = d * 200 / 180;
+
+            Console.WriteLine("Radyan = " + r);
+            Console.WriteLine("Grad = " + g);
+            Console.ReadKey();
+
+        }
+    }
+}
